94e930316d83f6b530ee16154257e90d21334714
duy <duyhet2005@gmail.com>
2025-11-04 16:02:34 +0700

    update

---
+++ client/src/pages/Houses.jsx
@@ -0,0 +1,2 @@
+import ListingBase from './_ListingBase.jsx'
+export default function Houses(){ return <ListingBase category="nha-nguyen-can" title="Nhà nguyên căn" /> }
---
+++ client/src/pages/Rooms.jsx
@@ -0,0 +1,2 @@
+import ListingBase from './_ListingBase.jsx'
+export default function Rooms(){ return <ListingBase category="phong-tro" title="Phòng/ Nhà trọ" /> }
